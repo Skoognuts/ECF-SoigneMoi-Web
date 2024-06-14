308f77c61765fe8feece8ce1cdda3ed9d3a80d11
Julien Labatut <j.labatut33127@gmail.com>
2024-06-14 12:43:57 +0200

    Create auth route

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Redirection automatique en fonction du rôle
+        if ($this->getUser()) {
+            if (in_array('ROLE_ADMIN', $this->getUser()->getRoles(), true)) {
+                return $this->redirectToRoute('app_admin');
+            } else if (in_array('ROLE_DOCTOR', $this->getUser()->getRoles(), true)) {
+                return $this->redirectToRoute('app_access_denied');
+            } else if (in_array('ROLE_SECRETARY', $this->getUser()->getRoles(), true)) {
+                return $this->redirectToRoute('app_access_denied');
+            } else if (in_array('ROLE_USER', $this->getUser()->getRoles(), true)) {
+                return $this->redirectToRoute('app_main');
+            }
+        }
+
+        // Redirection apres erreur d'authentification
+        $error = $authenticationUtils->getLastAuthenticationError();
+        if ($error) {
+            return $this->redirectToRoute('app_login_error', [], Response::HTTP_SEE_OTHER);
+        }
+
+        // Configuration du formulaire de connexion avec les derniers identifiants connus
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // Rendu de la page 
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    #[Route('/access-denied', name: 'app_access_denied')]
+    public function index(): Response
+    {
+        return $this->render('security/access_denied.html.twig', []);
+    }
+
+    // Route de déconnexion
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): Response
+    {
+        // Déconnexion
+        return $this->redirectToRoute('app_landing_page', [], Response::HTTP_SEE_OTHER);
+    }
+
+    // Route d'erreur d'identifiants (temporisation)
+    #[Route(path: '/login-error', name: 'app_login_error')]
+    public function loginError(): Response
+    {
+        // Rendu de la page 
+        return $this->render('security/login_error.html.twig');
+    }
+}
---
+++ src/Security/LoginAuthenticator.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class LoginAuthenticator extends AbstractLoginFormAuthenticator
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+
+    // Constructeur de classe
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    // Fonction d'authentification
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->getPayload()->getString('email');
+
+        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($request->getPayload()->getString('password')),
+            [
+                new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
+                new RememberMeBadge(),
+            ]
+        );
+    }
+
+    // Fonction de redirection en cas de succès à l'authentification
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        if (in_array('ROLE_ADMIN', $token->getUser()->getRoles(), true)) {
+            return new RedirectResponse($this->urlGenerator->generate('app_admin'));
+        } else if (in_array('ROLE_DOCTOR', $token->getUser()->getRoles(), true)) {
+            return new RedirectResponse($this->urlGenerator->generate('app_access_denied'));
+        } else if (in_array('ROLE_SECRETARY', $token->getUser()->getRoles(), true)) {
+            return new RedirectResponse($this->urlGenerator->generate('app_access_denied'));
+        } else if (in_array('ROLE_USER', $token->getUser()->getRoles(), true)) {
+            return new RedirectResponse($this->urlGenerator->generate('app_main'));
+        } 
+    }
+
+    // Récupération de l'Url de connexion
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
