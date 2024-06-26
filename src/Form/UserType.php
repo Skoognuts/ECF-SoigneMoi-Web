3b20214a47b9e15a276514e518597f26fb0b4448
Julien Labatut <j.labatut33127@gmail.com>
2024-06-26 15:02:38 +0200

    Add admin crud files

---
+++ src/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Specialty;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('password')
+            ->add('roles')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('address')
+            ->add('registration_number')
+            ->add('specialty', EntityType::class, [
+                'class' => Specialty::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
