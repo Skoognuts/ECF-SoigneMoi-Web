5c9de11567ff49337f94000c446d182dfa161c92
Julien Labatut <j.labatut33127@gmail.com>
2024-06-27 09:17:10 +0200

    Update doctor creation template

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AdminController extends AbstractController
@@ -26,29 +27,119 @@ class AdminController extends AbstractController
         ]);
     }
 
+    // Route de création d'un docteur
     #[Route('/admin/new', name: 'app_admin_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasher): Response
     {
+        // Déclaration des variables
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
+        $fakeEmailError = false;
+        $existingUserError = false;
+        $matriculeError = false;
+        $existingMatriculeError = false;
 
+        // Controle du clic
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($user);
-            $entityManager->flush();
+            // Mise en forme des propriétés simples
+            $user->setFirstName(ucwords($form->get('firstname')->getData()));
+            $user->setLastName(strtoupper($form->get('lastname')->getData()));
 
-            return $this->redirectToRoute('app_admin_index', [], Response::HTTP_SEE_OTHER);
+            // Gestion du regex de l'adresse mail
+            if (!filter_var($form->get('email')->getData(), FILTER_VALIDATE_EMAIL)) {
+                $fakeEmailError = true;
+                return $this->render('admin/new.html.twig', [
+                    'user' => $user,
+                    'form' => $form,
+                    'fakeEmailError' => $fakeEmailError,
+                    'existingUserError' => $existingUserError,
+                    'matriculeError' => $matriculeError,
+                    'existingMatriculeError' => $existingMatriculeError
+                ]);
+            }
+
+            // Gestion d'adresse mail déjà associée à un compte
+            $existingUsers = $userRepository->findAll();
+            $existingUsersMails = [];
+            foreach ($existingUsers as $key => $existingUser) {
+                array_push($existingUsersMails, $existingUser->getEmail());
+            }
+            foreach ($existingUsersMails as $key => $mail) {
+                if ($mail == $form->get('email')->getData()) {
+                    $existingUserError = true;
+                    return $this->render('admin/new.html.twig', [
+                        'user' => $user,
+                        'form' => $form,
+                        'fakeEmailError' => $fakeEmailError,
+                        'existingUserError' => $existingUserError,
+                        'matriculeError' => $matriculeError,
+                        'existingMatriculeError' => $existingMatriculeError
+                    ]);
+                }
+            }
+
+            // Gestion de la longueure du matricule
+            $registrationNumber = $form->get('registration_number')->getData();
+            $registrationNumberLength = strlen((string)$registrationNumber);
+            if ($registrationNumberLength != 7) {
+                $matriculeError = true;
+                return $this->render('admin/new.html.twig', [
+                    'user' => $user,
+                    'form' => $form,
+                    'fakeEmailError' => $fakeEmailError,
+                    'existingUserError' => $existingUserError,
+                    'matriculeError' => $matriculeError,
+                    'existingMatriculeError' => $existingMatriculeError
+                ]);
+            }
+
+            // Gestion de matricule déjà associé à un docteur
+            $doctors = $userRepository->findDoctors();
+            foreach ($doctors as $key => $doctor) {
+                if ($doctor->getRegistrationNumber() == $registrationNumber) {
+                    $existingMatriculeError = true;
+                    return $this->render('admin/new.html.twig', [
+                        'user' => $user,
+                        'form' => $form,
+                        'fakeEmailError' => $fakeEmailError,
+                        'existingUserError' => $existingUserError,
+                        'matriculeError' => $matriculeError,
+                        'existingMatriculeError' => $existingMatriculeError
+                    ]);
+                }
+            }
+
+            // Hashage du mot de passe
+            $user->setPassword(
+                $userPasswordHasher->hashPassword($user, $form->get('plainPassword')->getData())
+            );
+
+            // Définition du rôle de l'utilisateur
+            $user->setRoles(['ROLE_DOCTOR']);
+
+            $userRepository->save($user, true);
+
+            // Redirection après la création du docteur
+            return $this->redirectToRoute('app_admin', [], Response::HTTP_SEE_OTHER);
         }
 
+        // Rendu du formulaire
         return $this->render('admin/new.html.twig', [
             'user' => $user,
             'form' => $form,
+            'fakeEmailError' => $fakeEmailError,
+            'existingUserError' => $existingUserError,
+            'matriculeError' => $matriculeError,
+            'existingMatriculeError' => $existingMatriculeError
         ]);
     }
 
+    // Route d'affichage d'un docteur
     #[Route('/admin/{id}', name: 'app_admin_show', methods: ['GET'])]
     public function show(User $user): Response
     {
+        // Rendu de la page
         return $this->render('admin/show.html.twig', [
             'user' => $user,
         ]);
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,12 @@ namespace App\Form;
 
 use App\Entity\Specialty;
 use App\Entity\User;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,16 +20,50 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('roles')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('address')
-            ->add('registration_number')
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-text-input required',
+                ],
+                'label' => 'E-mail'
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'required' => true,
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-text-input required'
+                ],
+                'label' => 'Mot de passe',
+            ])
+            ->add('firstname', TextType::class, [
+                'required' => true,
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => 'form-text-input required'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'required' => true,
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => 'form-text-input required'
+                ]
+            ])
+            ->add('registration_number', NumberType::class, [
+                'required' => true,
+                'label' => 'Matricule',
+                'attr' => [
+                    'class' => 'form-text-input required'
+                ]
+            ])
             ->add('specialty', EntityType::class, [
+                'required' => true,
                 'class' => Specialty::class,
-                'choice_label' => 'id',
+                'label' => 'Specialité',
+                'placeholder' => 'Choisir une spécialté',
+                'attr' => [
+                    'class' => 'form-text-input required'
+                ]
             ])
         ;
     }
